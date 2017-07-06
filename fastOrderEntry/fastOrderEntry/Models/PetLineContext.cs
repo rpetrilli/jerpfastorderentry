04dbfc01348853f853aea4043b31d7a45c1b1c2d
saverio belviso <sbelviso@gmail.com>
2017-07-06 16:50:22 +0200

    ok

--- fastOrderEntry/fastOrderEntry/Models/PetLineContext.cs
+++ fastOrderEntry/fastOrderEntry/Models/PetLineContext.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
 
 namespace fastOrderEntry.Models
 {
     public class PetLineContext: DbContext
     {
+        static PetLineContext()
+        {
+            //non modifica lo schema del database
+            Database.SetInitializer<PetLineContext>(null);
+        }
+
         public PetLineContext() : base(nameOrConnectionString: "DefaultConnectionString") { }
+
         public virtual DbSet<ca_divisa> ca_divisa { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBulider)
+        {
+            modelBulider.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBulider);
+        }
     }
 }
