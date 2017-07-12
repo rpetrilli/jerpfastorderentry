a4671572ea7f0da6dd35aea3c050dd9d9b2b5e65
roberto.petrilli@implementa-srl.it <roberto.petrilli@implementa-srl.it>
2017-07-13 00:13:08 +0200

    gestione login

---
+++ fastOrderEntry/fastOrderEntry/Controllers/AuthController.cs
@@ -0,0 +1,76 @@
+﻿using fastOrderEntry.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fastOrderEntry.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+        private NpgsqlConnection con = null;
+
+        public ActionResult Login(string returnUrl)
+        {
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) //Checks if input fields have the correct format
+            {
+                ModelState.AddModelError("LoginMessage", "Specificare nome utente e password");
+                return View(model); //Returns the view with the input values so that the user doesn't have to retype again
+            }
+
+
+
+            if (model.nomeUtente == "admin" && model.password == "admin")
+            {
+                var identity = new ClaimsIdentity( new[] {
+                    new Claim(ClaimTypes.Name, "Amministratore"),
+                    new Claim(ClaimTypes.NameIdentifier, "admin"),
+                }, "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            ModelState.AddModelError("LoginMessage", "Nome utente o password non validi");
+
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Login", "Auth");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "listiniArticolo");
+            }
+            return returnUrl;
+        }
+    }
+
+
+}
---
+++ fastOrderEntry/fastOrderEntry/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace fastOrderEntry.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string nomeUtente { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
